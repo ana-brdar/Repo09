77554ee7fc6e50de470f73f60ada1b307f473537
ana-brdar <brdar.ana@yahoo.com>
2022-04-06 08:06:32 +0200

    Do...While loops

---
+++ ex19/src/index.js
@@ -0,0 +1,16 @@
+function myDoWhile() {
+    var myNumbers = "";
+    let i = 0;
+    do {
+      myNumbers += i + ", ";
+      i++;
+    } while (i < 9);
+    {
+      i === 9 ? (myNumbers += i) : (myNumbers += i + ", ");
+    }
+    return myNumbers;
+  }
+  
+  console.log(myDoWhile());
+  
+  module.exports = myDoWhile;
